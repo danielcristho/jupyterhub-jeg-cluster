acf241f71d8ce7e462e6f94ec45da5dc3514c05b
danielcristho <pepuhodaniel93.learn@gmail.com>
2025-05-18 00:03:36 +0700

    feat: add CORS config

--- discovery-api/app.py
+++ discovery-api/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import redis
 import time, os, requests
 from dotenv import load_dotenv
 
 app = Flask(__name__)
+CORS(app, origins="*")
 load_dotenv()
 
 """Redis Configuration"""
