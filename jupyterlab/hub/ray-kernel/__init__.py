ed5633b6489a5d876a6fd5b93f1dbf07f256fcfd
danielcristho <pepuhodaniel93.learn@gmail.com>
2025-08-06 14:54:03 +0700

    adjust: remove qemu dir

--- jupyterlab/hub/ray-kernel/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Ray Kernel Package"""
-
-from .ray_kernel import RayClusterKernel, RayClusterKernelApp
-
-__version__ = '1.0.0'
-__all__ = ['RayClusterKernel', 'RayClusterKernelApp']
--- jupyterlab/hub/ray-kernel/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Main entry point for Ray kernel"""
-
-from .ray_kernel import RayClusterKernelApp
-
-if __name__ == '__main__':
-    RayClusterKernelApp.launch_instance()
